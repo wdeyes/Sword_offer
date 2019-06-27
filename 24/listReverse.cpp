57ad7a2eed4e5828e63c023b6cdedf39b85b316b
wdeye <wd_wangdong@foxmail.com>
2019-06-27 22:41:07 +0800

    finish 24

---
+++ 24/listReverse.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+#include"my_list.h"
+using namespace std;
+
+ListNode* ReverseList(ListNode* pHead){
+	if(pHead==nullptr)
+		return nullptr;
+	ListNode* pFront=nullptr;
+	ListNode* pCur=pHead;
+	ListNode* ret = nullptr;
+	
+
+	while(pCur!=nullptr){
+		ListNode* pBack = pCur->m_pNext;
+		pCur->m_pNext = pFront;
+
+		if(pBack==nullptr)
+			ret = pCur;
+
+		pFront=pCur;
+		pCur=pBack;
+	}
+	return ret;
+}
+
+
+
+
+// ====================测试代码====================
+ListNode* Test(ListNode* pHead)
+{
+    printf("The original list is: \n");
+    PrintList(pHead);
+
+    ListNode* pReversedHead = ReverseList(pHead);
+
+    printf("The reversed list is: \n");
+    PrintList(pReversedHead);
+
+    return pReversedHead;
+}
+
+// 输入的链表有多个结点
+void Test1()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(2);
+    ListNode* pNode3 = CreateListNode(3);
+    ListNode* pNode4 = CreateListNode(4);
+    ListNode* pNode5 = CreateListNode(5);
+
+    ConnectListNodes(pNode1, pNode2);
+    ConnectListNodes(pNode2, pNode3);
+    ConnectListNodes(pNode3, pNode4);
+    ConnectListNodes(pNode4, pNode5);
+
+    ListNode* pReversedHead = Test(pNode1);
+
+    DestroyList(pReversedHead);
+}
+
+// 输入的链表只有一个结点
+void Test2()
+{
+    ListNode* pNode1 = CreateListNode(1);
+
+    ListNode* pReversedHead = Test(pNode1);
+
+    DestroyList(pReversedHead);
+}
+
+// 输入空链表
+void Test3()
+{
+    Test(nullptr);
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+
+    return 0;
+}
+
