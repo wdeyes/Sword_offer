0fbafc18cbf49bd92d9555812000f4f240fd60d9
wdeye <wd_wangdong@foxmail.com>
2019-06-29 22:32:51 +0800

    finish 29

---
+++ 29/print_matrix.cpp
@@ -0,0 +1,153 @@
+#include<iostream>
+using namespace std;
+
+void PrintACircle(int** numbers, int x, int cols, int rows){
+	for(int i=x; i<(cols+x); ++i){
+		cout << numbers[x][i] << " ";
+	}
+	for(int i=x+1; i<(rows+x); ++i){
+		cout << numbers[i][x+cols-1] << " ";
+	}
+	if(rows!=1 && cols!=1)
+		for(int i=(cols-2+x); i>=x; --i){
+			cout << numbers[x+rows-1][i] << " ";
+		}
+	if(rows!=1 && cols!=1)
+		for(int i=(rows-2+x); i>x; --i){
+			cout << numbers[i][x] << " ";
+		}
+}
+
+void PrintMatrixClockwisely(int** numbers, int columns, int rows){
+	int small = columns<rows? columns:rows;
+	for (int i = 0; i < ((small+1)/2); ++i)
+	{
+		PrintACircle(numbers, i, columns-2*i, rows-2*i);
+	}
+}
+
+
+
+
+// ====================测试代码====================
+void Test(int columns, int rows)
+{
+    printf("Test Begin: %d columns, %d rows.\n", columns, rows);
+
+    if(columns < 1 || rows < 1)
+        return;
+
+    int** numbers = new int*[rows];
+    for(int i = 0; i < rows; ++i)
+    {
+        numbers[i] = new int[columns];
+        for(int j = 0; j < columns; ++j)
+        {
+            numbers[i][j] = i * columns + j + 1;
+        }
+    }
+
+    PrintMatrixClockwisely(numbers, columns, rows);
+    printf("\n");
+
+    for(int i = 0; i < rows; ++i)
+        delete[] (int*)numbers[i];
+
+    delete[] numbers;
+}
+
+int main(int argc, char* argv[])
+{
+    /*
+    1    
+    */
+    Test(1, 1);
+
+    /*
+    1    2
+    3    4
+    */
+    Test(2, 2);
+
+    /*
+    1    2    3    4
+    5    6    7    8
+    9    10   11   12
+    13   14   15   16
+    */
+    Test(4, 4);
+
+    /*
+    1    2    3    4    5
+    6    7    8    9    10
+    11   12   13   14   15
+    16   17   18   19   20
+    21   22   23   24   25
+    */
+    Test(5, 5);
+
+    /*
+    1
+    2
+    3
+    4
+    5
+    */
+    Test(1, 5);
+
+    /*
+    1    2
+    3    4
+    5    6
+    7    8
+    9    10
+    */
+    Test(2, 5);
+
+    /*
+    1    2    3
+    4    5    6
+    7    8    9
+    10   11   12
+    13   14   15
+    */
+    Test(3, 5);
+
+    /*
+    1    2    3    4
+    5    6    7    8
+    9    10   11   12
+    13   14   15   16
+    17   18   19   20
+    */
+    Test(4, 5);
+
+    /*
+    1    2    3    4    5
+    */
+    Test(5, 1);
+
+    /*
+    1    2    3    4    5
+    6    7    8    9    10
+    */
+    Test(5, 2);
+
+    /*
+    1    2    3    4    5
+    6    7    8    9    10
+    11   12   13   14    15
+    */
+    Test(5, 3);
+
+    /*
+    1    2    3    4    5
+    6    7    8    9    10
+    11   12   13   14   15
+    16   17   18   19   20
+    */
+    Test(5, 4);
+
+    return 0;
+}
+
