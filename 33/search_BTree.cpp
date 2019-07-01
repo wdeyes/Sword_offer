9779437edb0013c4736ca1c369b2beedb64df12d
wdeye <wd_wangdong@foxmail.com>
2019-07-01 22:16:51 +0800

    finish 33

---
+++ 33/search_BTree.cpp
@@ -0,0 +1,134 @@
+#include<iostream>
+#include <assert.h>
+using namespace std;
+
+
+bool core(int data[], int start, int end){
+	if(start>=end)
+		return true;
+
+	int p=start;
+	bool ret=false;
+	while(data[p]<data[end] && p<=end){
+		++p;
+	}
+	for(int i=p;i<end;++i){
+		if(data[i]<data[end])
+			return false;
+	}
+	ret=core(data, start, p-1) && core(data, p, end-1);
+	return ret;
+}
+
+bool VerifySquenceOfBST(int data[], int length){
+	if(data==nullptr || length<=0)
+		return false;
+	return core(data, 0, length-1);
+}
+
+
+
+
+
+
+// ====================测试代码====================
+void Test(const char* testName, int sequence[], int length, bool expected)
+{
+    if(testName != nullptr)
+        printf("%s begins: ", testName);
+
+    if(VerifySquenceOfBST(sequence, length) == expected)
+        printf("passed.\n");
+    else
+        printf("failed.\n");
+}
+
+//            10
+//         /      \
+//        6        14
+//       /\        /\
+//      4  8     12  16
+void Test1()
+{
+    int data[] = {4, 8, 6, 12, 16, 14, 10};
+    Test("Test1", data, sizeof(data)/sizeof(int), true);
+}
+
+//           5
+//          / \
+//         4   7
+//            /
+//           6
+void Test2()
+{
+    int data[] = {4, 6, 7, 5};
+    Test("Test2", data, sizeof(data)/sizeof(int), true);
+}
+
+//               5
+//              /
+//             4
+//            /
+//           3
+//          /
+//         2
+//        /
+//       1
+void Test3()
+{
+    int data[] = {1, 2, 3, 4, 5};
+    Test("Test3", data, sizeof(data)/sizeof(int), true);
+}
+
+// 1
+//  \
+//   2
+//    \
+//     3
+//      \
+//       4
+//        \
+//         5
+void Test4()
+{
+    int data[] = {5, 4, 3, 2, 1};
+    Test("Test4", data, sizeof(data)/sizeof(int), true);
+}
+
+// 树中只有1个结点
+void Test5()
+{
+    int data[] = {5};
+    Test("Test5", data, sizeof(data)/sizeof(int), true);
+}
+
+void Test6()
+{
+    int data[] = {7, 4, 6, 5};
+    Test("Test6", data, sizeof(data)/sizeof(int), false);
+}
+
+void Test7()
+{
+    int data[] = {4, 6, 12, 8, 16, 14, 10};
+    Test("Test7", data, sizeof(data)/sizeof(int), false);
+}
+
+void Test8()
+{
+    Test("Test8", nullptr, 0, false);
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+    Test6();
+    Test7();
+    Test8();
+
+    return 0;
+}
