b80a0edf7795211dffe213953bb3c51ba8812d37
wdeye <wd_wangdong@foxmail.com>
2019-07-19 22:20:23 +0800

    finish 59

---
+++ 59/slidWindow.cpp
@@ -0,0 +1,197 @@
+#include<iostream>
+#include <vector>
+#include <deque>
+using namespace std;
+
+
+vector<int> maxInWindows(const vector<int> num, unsigned int size){
+	vector<int> ret;
+	if(size<=0 || num.size()<size)
+		return ret;
+
+	deque<int> ans;
+	for(int i=0; i<size; ++i){
+		while(!ans.empty() && num[ans.front()]<num[i])
+			ans.pop_front();
+		ans.push_back(i);
+	}
+	ret.push_back(num[ans.front()]);
+
+	for(int i=size; i<num.size(); ++i){
+		if(i-ans.front()>=size){
+			ans.pop_front();
+		}
+		// 没有必要从前往后删，因为ans对应的num肯定是递减的。
+		// while(!ans.empty() && num[ans.front()]<=num[i])
+		// 	ans.pop_front();
+		while(!ans.empty() && num[ans.back()]<=num[i])
+			ans.pop_back();
+		ans.push_back(i);
+		ret.push_back(num[ans.front()]);
+	}
+
+	return ret;
+}
+
+
+
+
+
+// ====================测试代码====================
+void Test(const char* testName, const vector<int>& num, unsigned int size, const vector<int>& expected)
+{
+    if(testName != nullptr)
+        printf("%s begins: ", testName);
+
+    vector<int> result = maxInWindows(num, size);
+
+    vector<int>::const_iterator iterResult = result.begin();
+    vector<int>::const_iterator iterExpected = expected.begin();
+    while(iterResult < result.end() && iterExpected < expected.end())
+    {
+        if(*iterResult != *iterExpected)
+            break;
+
+        ++iterResult;
+        ++iterExpected;
+    }
+
+    if(iterResult == result.end() && iterExpected == expected.end())
+        printf("Passed.\n");
+    else
+        printf("FAILED.\n");
+}
+
+void Test1()
+{
+    int num[] = { 2, 3, 4, 2, 6, 2, 5, 1 };
+    vector<int> vecNumbers(num, num + sizeof(num) / sizeof(int));
+
+    int expected[] = { 4, 4, 6, 6, 6, 5 };
+    vector<int> vecExpected(expected, expected + sizeof(expected) / sizeof(int));
+
+    unsigned int size = 3;
+
+    Test("Test1", vecNumbers, size, vecExpected);
+}
+
+void Test2()
+{
+    int num[] = { 1, 3, -1, -3, 5, 3, 6, 7 };
+    vector<int> vecNumbers(num, num + sizeof(num) / sizeof(int));
+
+    int expected[] = { 3, 3, 5, 5, 6, 7 };
+    vector<int> vecExpected(expected, expected + sizeof(expected) / sizeof(int));
+
+    unsigned int size = 3;
+
+    Test("Test2", vecNumbers, size, vecExpected);
+}
+
+// 输入数组单调递增
+void Test3()
+{
+    int num[] = { 1, 3, 5, 7, 9, 11, 13, 15 };
+    vector<int> vecNumbers(num, num + sizeof(num) / sizeof(int));
+
+    int expected[] = { 7, 9, 11, 13, 15 };
+    vector<int> vecExpected(expected, expected + sizeof(expected) / sizeof(int));
+
+    unsigned int size = 4;
+
+    Test("Test3", vecNumbers, size, vecExpected);
+}
+
+// 输入数组单调递减
+void Test4()
+{
+    int num[] = { 16, 14, 12, 10, 8, 6, 4 };
+    vector<int> vecNumbers(num, num + sizeof(num) / sizeof(int));
+
+    int expected[] = { 16, 14, 12 };
+    vector<int> vecExpected(expected, expected + sizeof(expected) / sizeof(int));
+
+    unsigned int size = 5;
+
+    Test("Test4", vecNumbers, size, vecExpected);
+}
+
+// 滑动窗口的大小为1
+void Test5()
+{
+    int num[] = { 10, 14, 12, 11 };
+    vector<int> vecNumbers(num, num + sizeof(num) / sizeof(int));
+
+    int expected[] = { 10, 14, 12, 11 };
+    vector<int> vecExpected(expected, expected + sizeof(expected) / sizeof(int));
+
+    unsigned int size = 1;
+
+    Test("Test5", vecNumbers, size, vecExpected);
+}
+
+// 滑动窗口的大小等于数组的长度
+void Test6()
+{
+    int num[] = { 10, 14, 12, 11 };
+    vector<int> vecNumbers(num, num + sizeof(num) / sizeof(int));
+
+    int expected[] = { 14 };
+    vector<int> vecExpected(expected, expected + sizeof(expected) / sizeof(int));
+
+    unsigned int size = 4;
+
+    Test("Test6", vecNumbers, size, vecExpected);
+}
+
+// 滑动窗口的大小为0
+void Test7()
+{
+    int num[] = { 10, 14, 12, 11 };
+    vector<int> vecNumbers(num, num + sizeof(num) / sizeof(int));
+
+    vector<int> vecExpected;
+
+    unsigned int size = 0;
+
+    Test("Test7", vecNumbers, size, vecExpected);
+}
+
+// 滑动窗口的大小大于输入数组的长度
+void Test8()
+{
+    int num[] = { 10, 14, 12, 11 };
+    vector<int> vecNumbers(num, num + sizeof(num) / sizeof(int));
+
+    vector<int> vecExpected;
+
+    unsigned int size = 5;
+
+    Test("Test8", vecNumbers, size, vecExpected);
+}
+
+// 输入数组为空
+void Test9()
+{
+    vector<int> vecNumbers;
+    vector<int> vecExpected;
+
+    unsigned int size = 5;
+
+    Test("Test9", vecNumbers, size, vecExpected);
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+    Test6();
+    Test7();
+    Test8();
+    Test9();
+
+    return 0;
+}
