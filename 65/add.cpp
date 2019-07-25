d2787b094e1d9936c8f372e3c4d4d75285a5815f
wdeye <wd_wangdong@foxmail.com>
2019-07-25 16:36:49 +0800

    finish 65

---
+++ 65/add.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int Add(int num1, int num2){
+	int sum = num1 ^ num2;
+	num2 = (num1 & num2)<<1;
+	num1=sum;
+	while(num2!=0){
+		sum = num1 ^ num2;
+		num2 = (num1 & num2)<<1;
+		num1 = sum;
+	}
+	return sum;
+}
+
+
+// ====================测试代码====================
+void Test(int num1, int num2, int expected)
+{
+    int result = Add(num1, num2);
+    if(result == expected)
+        printf("%d + %d is %d. Passed\n", num1, num2, result);
+    else
+        printf("%d + %d is %d. FAILED\n", num1, num2, result);
+}
+
+int main(int argc, char* argv[])
+{
+    Test(1, 2, 3);
+    Test(111, 899, 1010);
+
+    Test(-1, 2, 1);
+    Test(1, -2, -1);
+
+    Test(3, 0, 3);
+    Test(0, -4, -4);
+
+    Test(-2, -8, -10);
+
+    return 0;
+}
